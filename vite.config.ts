b50cfa72dbcb3972d1fc23cf0a9795871a760831
coder-msk <salmanmarwat06@gmail.com>
2026-03-20 02:04:47 +0500

    Update base URL for new repository

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Khewera-Salt-Co/',
+  base: '/Khewera-Salt-Co-2.0/',
   plugins: [react()],
 });
